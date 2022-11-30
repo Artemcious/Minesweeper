d36521692240ed229e7f4bd6a10975ec2694f53f
Artem <107361805+Artemcious@users.noreply.github.com>
2022-11-30 22:22:40 +0300

    First version

---
+++ Minesweeper/src/Field.java
@@ -0,0 +1,15 @@
+public class Field {
+
+    public void appRun() {
+
+    } // final method that merges everything
+
+    private void greetingsPlayer() {
+
+    }
+
+    private void creationField() {
+
+    }
+
+}
